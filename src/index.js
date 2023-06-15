6e34b5cf0a50726fa29e507ad696182ed26fec4a
Maitreya61 <maitreyaoffl@gmail.com>
2023-06-15 11:01:07 +0530

    twelfth commit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import {WorkoutContextProvider} from './context/WorkoutContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <React.StrictMode>
     <WorkoutContextProvider>
         <App />
     </WorkoutContextProvider>
+    </React.StrictMode>
   
 );
 
